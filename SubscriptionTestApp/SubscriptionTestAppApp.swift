7b23329f527d6f2e5350a8eca837aa60b87df275
emaksimova <maksimovaswift@gmail.com>
2025-12-02 06:08:29 +0300

    Initial Commit

---
+++ SubscriptionTestApp/SubscriptionTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionTestAppApp.swift
+//  SubscriptionTestApp
+//
+//  Created by Евгения Максимова on 02.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
